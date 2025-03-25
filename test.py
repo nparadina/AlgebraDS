5e0509020fbb3629a1e69afa5801add6c03a83b4
Nika Paradina <nika.paradina@gmail.com>
2025-03-25 20:19:59 +0100

    2050325

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-import sys
-print (sys.version)
+import numpy as np
+
+m1=np.arange(1,100,10)
+m2=np.arange(7,100,5)
+m3=np.linspace(1,4,100)
+m4=np.linspace(2,5,100)
+
+print(m1)
+m1=m1.reshape(2,5)
+print(m1)
+# print(m2)
+# print(m3)
+# print(m4)
